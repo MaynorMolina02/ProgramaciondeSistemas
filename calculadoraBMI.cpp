7fe198345d4b5e7b0e72f7c52c7a4ae3b6cbc444
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-04-25 21:15:53 -0600

    Create calculadoraBMI.cpp

---
+++ calculadoraBMI.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     double peso,altura,bmi;
+     cout<<"CALCULADORA DE INDICE DE MASA CORPORAL \n"<<endl;
+     cout<<"Ingrese el peso en kg: ";
+     cin>>peso;
+     cout<<"Ingrese la altura en metros: ";
+     cin>>altura;
+     
+     bmi = peso/(altura*altura);
+     cout<<"Su indice de masa corporal es: "<<bmi<<endl;
+     if(bmi<18.5){
+         cout<<"Bajo Peso"<<endl;
+     }
+     else if(bmi<25){
+         cout<<"Normal"<<endl;
+     }
+     else if(bmi<30){
+         cout<<"Sobrepeso"<<endl;
+     }
+    else{
+         cout<<"Obeso"<<endl;
+    }
+
+    return 0;
+}
