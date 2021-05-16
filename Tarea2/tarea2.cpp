11facf3cd8c91797a6ad55afda293cdf3dc259a2
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-05-15 19:12:52 -0600

    Tarea2

--- Tarea2/tarea2.cpp
+++ Tarea2/tarea2.cpp
@@ -14,19 +14,19 @@ int main(){
     int puntosEquipo1=6;
     int puntosEquipo2=6;
 
-    cout<<"Ingrese numero de partidos pendientes: ";
+    cout<<"Puntaje inicial de los equipos "<<endl;
+    cout<<"Equipo 1 = "<<puntosEquipo1<<endl<<"Equipo 2 = "<<puntosEquipo2<<endl<<endl;
+	cout<<"Ingrese número de partidos pendientes: ";
     cin>>partidosPendientes;
     
-    cout<<"Puntaje inicial de los equipos "<<endl;
-    cout<<"Equipo 1 = "<<puntosEquipo1<<endl<<"Equipo 2 = "<<puntosEquipo2<<endl;
     while(i<partidosPendientes){
           golesEquipo1= rand() % 10;
           golesEquipo2= rand() % 10;
-          cout<<endl<<"Goles Partido => ";
+          cout<<endl<<i+1<<". Goles Partido => ";
           cout<<"Equipo1="<<golesEquipo1<<" Equipo2="<<golesEquipo2<<endl;
           if(golesEquipo1 > golesEquipo2){
               puntosEquipo1 +=3;
-              cout<<"Resultado: Gano Equipo 1"<<endl;
+              cout<<"Resultado: Ganó Equipo 1"<<endl;
           }
           else if (golesEquipo1 == golesEquipo2){
               puntosEquipo1 +=1;
@@ -35,17 +35,17 @@ int main(){
           }
           else if (golesEquipo1 < golesEquipo2){
               puntosEquipo2 +=3;
-             cout<<"Resultado: Gano Equipo 2"<<endl;
+             cout<<"Resultado: Ganó Equipo 2"<<endl;
           }
          
         i++;
     }
      cout<<"\nPuntaje final "<<endl<<"Equipo 1 = "<<puntosEquipo1<<endl<<"Equipo 2 = "<<puntosEquipo2<<endl;
     if(puntosEquipo1 > puntosEquipo2){
-        cout<<"EL CAMPEON ES EL EQUIPO 1"<<endl;
+        cout<<"EL CAMPEÓN ES EL EQUIPO 1"<<endl;
     }
     else
-        cout<<"EL CAMPEON ES EL EQUIPO 2"<<endl;
+        cout<<"EL CAMPEÓN ES EL EQUIPO 2"<<endl;
     
     
     return 0;
