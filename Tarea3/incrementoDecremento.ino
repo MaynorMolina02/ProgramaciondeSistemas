778757aa9c10e2fcd5472d2698bde4174afb8821
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-05-19 23:25:30 -0600

    Update incrementoDecremento.ino

--- Tarea3/incrementoDecremento.ino
+++ Tarea3/incrementoDecremento.ino
@@ -1,34 +1,30 @@
-// C++ code
-//
 int num = 0;
-int incrementar=2;
-int decrementar=3;
+int btnIncrementar=2;
+int btnDecrementar=3;
 int ledRojo=5;
 int ledVerde=4;
 void setup()
 {
-  pinMode(incrementar, INPUT);
-  pinMode(decrementar, INPUT);
+  pinMode(btnIncrementar, INPUT);
+  pinMode(btnDecrementar, INPUT);
   pinMode(ledRojo, OUTPUT);
   pinMode(ledVerde, OUTPUT);
   Serial.begin(9600);
 }
-
 void loop()
 {
   Serial.print("Numero: ");
   Serial.println(num);
-  if(digitalRead(incrementar)){
+  if(digitalRead(btnIncrementar)){
      num++;
      digitalWrite(ledVerde, HIGH);
      delay(100); 
      digitalWrite(ledVerde, LOW);
   }
-  if(digitalRead(decrementar)){
+  if(digitalRead(btnDecrementar)){
      num--;
      digitalWrite(ledRojo, HIGH);
      delay(100); 
      digitalWrite(ledRojo, LOW);
   }
- 
 }
