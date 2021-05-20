ee08a53f9eccdb4466c42c15a1592a31d7f0b1f6
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-05-19 23:11:01 -0600

    tarea3

---
+++ Tarea3/incrementoDecremento.ino
@@ -0,0 +1,34 @@
+// C++ code
+//
+int num = 0;
+int push1=2;
+int push2=3;
+int ledRojo=5;
+int ledVerde=4;
+void setup()
+{
+  pinMode(push1, INPUT);
+  pinMode(push2, INPUT);
+  pinMode(ledRojo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print("Numero: ");
+  Serial.println(num);
+  if(digitalRead(push1)==1){
+     num++;
+     digitalWrite(ledVerde, HIGH);
+     delay(100); 
+     digitalWrite(ledVerde, LOW);
+  }
+  if(digitalRead(push2)==1){
+     num--;
+     digitalWrite(ledRojo, HIGH);
+     delay(100); 
+     digitalWrite(ledRojo, LOW);
+  }
+ 
+}
