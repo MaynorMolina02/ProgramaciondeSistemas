04f45938ae743e9751af3d9b4656667e6ad274b1
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-06-03 22:01:16 -0600

    Tarea6

--- Tarea6/herencia.cpp
+++ Tarea6/herencia.cpp
@@ -4,25 +4,49 @@ using namespace std;
 //calcular costos por Noche
 class Hospedaje{
 public:
-      Hospedaje(){
-
+      Hospedaje(int _costo){
+            costoHospdaje = _costo;
       }
- 
-      
+      int obtenerCosto(){
+           return costoHospdaje;
+      }
+
+ private:
+      int costoHospdaje;
 };
 
 class Hoteles: public Hospedaje{
    public:
+          Hoteles(int _costo,string _habitaciones,string _comida): Hospedaje(_costo){
+           numHabitaciones = _habitaciones;
+           servicioComida = _comida;
+          }
+
+       int calcularCostos(){
+              int costos = 0;
+              costos = obtenerCosto() * numHabitaciones;
+              if(servicioComida == "Almuerzo"){
+                costos +=100;
+              }else  if(servicioComida == "Desayuno"){
+                costos +=50;
+              }else  if(servicioComida == "Cena"){
+                costos +=80;
+              }
+              return costos;
+       }
 
    private:
    //desayuno, almuerzo y cena
-          int Habitaciones;
-          int servicioComida;
+          int numHabitaciones;
+          string servicioComida;
 
 };
 
 class Apartamentos: public Hospedaje{
-
+  public:
+         Apartamentos(int _costo, int _tiempoRenta):Hospedaje(_costo){
+             tiempoRenta = _tiempoRenta;
+         }
   private:
          //rentarse mau
          int tiempoRenta;
@@ -30,7 +54,10 @@ class Apartamentos: public Hospedaje{
 
 class AirBnb: public Hospedaje{
    public: 
-          AirBnb()
+          AirBnb(int _costo, int _personas):Hospedaje(_costo){
+           numPersonas = _personas;
+          }
+
    private:
         int numPersonas;
 };
@@ -38,6 +65,7 @@ class AirBnb: public Hospedaje{
 
 
 int main(){
+    Hoteles hotel(100,2."Almuerz0");
 
     return 0;
-}
+}_
