6c0016e49d0ff77bdc95f48db16556a5781b776c
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-06-07 13:11:45 -0600

    Tarea7

--- Tarea7/apuntadores.cpp
+++ Tarea7/apuntadores.cpp
@@ -5,16 +5,21 @@ using namespace std;
 void mostrarFechaHora (int anio, int mes, int dia, int hora, int minuto){
   string m[12] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
                   "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
-  if(mes<=12 && hora<=23 && hora>=0 && minuto<=59 && minuto>=0){
-   cout<<dia<<" de "<<m[mes-1]<<" de "<< anio <<", "<<hora<<":"<<minuto<<endl;}
+  if(mes<=12 && mes>=1){
+    if(hora<=23 && hora>=0){
+      if( minuto<=59 && minuto>=0){
+         cout<<dia<<" de "<<m[mes-1]<<" de "<< anio <<", "<<hora<<":"<<minuto<<endl;
+       }
+        else
+        cout<<"Minutos incorrectos";
+      }
+  }
    else
-   cout<<"Fecha incorrecta";
+   cout<<"Mes incorrecto";
 }
 
 int main(){
-    char buffer[33];
-   int num = 5;
-   itoa(num,buffer,10);
+  
  mostrarFechaHora(2016,13,15,13,30);
 
 
