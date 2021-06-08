ea4aabc52e4ea61fcfce00ab0ca411545e6fb50c
MaynorMolina02 <83190197+MaynorMolina02@users.noreply.github.com>
2021-06-07 23:06:44 -0600

    Tarea7

--- Tarea7/apuntadores.cpp
+++ Tarea7/apuntadores.cpp
@@ -18,89 +18,103 @@ void mostrarFechaHora (int anio, int mes, int dia, int hora, int minuto){
   string m[12] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio",
                   "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
   if(mes<=12 && mes>=1){
+   if(dia<=31 && dia>=1){
     if(hora<=23 && hora>=0){
       if( minuto<=59 && minuto>=0){
          string mesC =  m[mes-1];
          char mesF[11];
-         strcpy(mesF,mesC.c_str());
-
           char diaC[3];
-          itoa(dia,diaC,10);
-
           char anioC[5];
+          char horaC[3];
+          char minutoC[3];
+
+          strcpy(mesF,mesC.c_str());
+          itoa(dia,diaC,10);
           itoa(anio,anioC,10);
-
-            char horaC[3];
           itoa(hora,horaC,10);
-
-            char minutoC[3];
           itoa(minuto,minutoC,10);
 
-        cout<<dia<<" de "<<m[mes-1]<<" de "<< anio <<", "<<hora<<":"<<minuto<<endl;
-         char fecha[90];
+         char *me;
+         char *d;
+         char *a;
+         char *h;
+         char *min;
+         me = mesF; 
+         d = diaC;
+         a = anioC;
+         h = horaC;
+         min = minutoC;
+
+         char fecha[35];
  
-         
          int j=0;
          int i=0;
 
-         for(i=0; i<longitudCadena(diaC); i++){
-            fecha[j] = diaC[i];
+          while(*d != '\0'){
+            fecha[j] = *d++;
             j++;
          }
+        
          fecha[j] =' ';
          fecha[j+1] = 'd';
          fecha[j+2] = 'e';
          fecha[j+3] = ' ';
 
          j+=4;
-         for(i=0; i<longitudCadena(mesF); i++){
-            fecha[j] = mesF[i];
+     
+         while(*me != '\0'){
+            fecha[j] = *me++;
             j++;
-         }
-            fecha[j] =' ';
+         } 
+
+         fecha[j] =' ';
          fecha[j+1] = 'd';
          fecha[j+2] = 'e';
          fecha[j+3] = ' ';
 
          j+=4;
-          for(i=0; i<longitudCadena(anioC); i++){
-            fecha[j] = anioC[i];
+         while(*a != '\0'){
+            fecha[j] = *a++;
             j++;
          }
 
          fecha[j] = ',';
          fecha[j+1] = ' ';
          j+=2;
-          for(i=0; i<longitudCadena(horaC); i++){
-            fecha[j] = horaC[i];
+         while(*h != '\0'){
+            fecha[j] = *h++;
             j++;
          }
           fecha[j] =':';
         j++;
-          for(i=0; i<longitudCadena(minutoC); i++){
-            fecha[j] = minutoC[i];
+          while(*min != '\0'){
+            fecha[j] = *min++;
             j++;
          }
 
-         cout<<fecha<<endl;
+         cout<<"La fecha y hora es: "<<fecha<<endl;
        }
         else
-        cout<<"Minutos incorrectos";
+        cout<<"Minutos incorrectos"<<endl;
       }
       else
-          cout<<"Hora incorrecta";
+          cout<<"Hora incorrecta"<<endl;
+            }
+   else
+   cout<<"Dia incorrecto"<<endl;
   }
    else
-   cout<<"Mes incorrecto";
+   cout<<"Mes incorrecto"<<endl;
 }
 
 
 
 int main(){
-  
- mostrarFechaHora(2016,11,15,7,50);
- char c[40] = "CadenaPrueba";
- cout<<"Tamano de cadena: "<<longitudCadena(c)<<endl;
+
+ mostrarFechaHora(2016,9,24,23,30);
+
+ char c[40] = "Cadena de prueba";
+ cout<<"El tamano de la cadena '"<<c<<"' es: "<<longitudCadena(c)<<endl;
 
     return 0;
 }
